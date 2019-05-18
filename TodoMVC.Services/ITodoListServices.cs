9fd3e26d895529255003b6953c21e062a2154e98
Artush Hakobyan <artush.hakobyan3@gmail.com>
2019-05-18 10:58:24 -0700

    add services layer

---
+++ TodoMVC.Services/ITodoListServices.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using TodoMVC.Data.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace TodoMVC.Services
+{
+    public interface ITodoListServices
+    {
+        Task AddAsync(TodoItem todoItem);
+
+        Task<IEnumerable<TodoItem>> GetAllAsync(int page, int pageCount);
+
+        Task<TodoItem> GetByIdAsync(Guid id);
+
+        Task RemoveAsync(Guid id);
+
+        Task EditAsync(TodoItem todoItem);
+    }
+}
---
+++ TodoMVC.Services/TodoListServices.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TodoMVC.Data;
+using TodoMVC.Data.Models;
+
+namespace TodoMVC.Services
+{
+    public class TodoListServices : ITodoListServices
+    {
+        private readonly TodoListDbContext _context;
+
+        public TodoListServices(TodoListDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddAsync(TodoItem todoItem)
+        {
+            await _context.TodoItems.AddAsync(todoItem);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task EditAsync(TodoItem todoItem)
+        {
+            TodoItem item = _context.TodoItems.SingleOrDefault(x => x.Id == todoItem.Id);
+
+            if(item != null)
+            {
+                item.IsCompleted = todoItem.IsCompleted;
+                item.Title = todoItem.Title;
+                _context.TodoItems.Update(item);
+                await _context.SaveChangesAsync();
+            }
+        }
+
+        public async Task<IEnumerable<TodoItem>> GetAllAsync(int page, int pageCount)
+        {
+            IEnumerable<TodoItem> result = _context.TodoItems.ToList();
+
+            result = result.Skip(page * pageCount).Take(pageCount);
+
+            return result;
+        }
+
+        public async Task<TodoItem> GetByIdAsync(Guid id)
+        {
+            TodoItem item = _context.TodoItems.SingleOrDefault(x => x.Id == id);
+
+            return item;
+        }
+
+        public async Task RemoveAsync(Guid id)
+        {
+            TodoItem item = _context.TodoItems.SingleOrDefault(x => x.Id == id);
+
+            if(item != null)
+            {
+                _context.TodoItems.Remove(item);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
