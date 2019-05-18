fbc901b040b664863fdd14beab53e43dbe864bbd
Artush Hakobyan <artush.hakobyan3@gmail.com>
2019-05-18 11:03:51 -0700

    add todo list services extension method

---
+++ TodoMVC.Services/TodoListServicesExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TodoMVC.Services
+{
+    public static class TodoListServicesExtensions
+    {
+        public static IServiceCollection AddTodoList(this IServiceCollection serviceCollection)
+        {
+            return serviceCollection.AddScoped<ITodoListServices, TodoListServices>();
+        }
+    }
+}
